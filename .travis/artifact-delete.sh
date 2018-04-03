7ba300b5cf1220b01570fee1732af6f133f8e450
Gerd Wütherich <gerd@gerd-wuetherich.de>
2018-04-03 13:42:35 +0200

    ~ fixed delete

--- .travis/artifact-delete.sh
+++ .travis/artifact-delete.sh
@@ -9,27 +9,27 @@ target_dir=slizaa-rcp-workbench
 for i in `curl --ssl -k -s -l ftp://"$user":"$password"@"$ftp_ip/$target_dir" | grep .jar`; do
 {
        echo "deleting $i";
-       curl --ssl -k ftp://${ftp_ip}:${ftp_port}/$target_dir/${i} -u "$user:$password" -O --quote "DELE ${i}"
+       curl --ssl -k ftp://"$user:$password"@"${ftp_ip}:${ftp_port}/$target_dir/${i}" -O --quote "DELE ${i}"
 };
 done;
 
 for i in `curl --ssl -k -s -l ftp://"$user":"$password"@"$ftp_ip/$target_dir/plugins/" | grep .jar`; do
 {
        echo "deleting plugins/$i";
-       curl --ssl -k ftp://${ftp_ip}:${ftp_port}/$target_dir/plugins/${i} -u "$user:$password" -O --quote "DELE $target_dir/plugins/${i}"
+       curl --ssl -k ftp://"$user:$password"@"${ftp_ip}:${ftp_port}/$target_dir/plugins/${i}" -O --quote "DELE $target_dir/plugins/${i}"
 };
 done;
 
 for i in `curl --ssl -k -s -l ftp://"$user":"$password"@"$ftp_ip/$target_dir/features/" | grep .jar`; do
 {
        echo "deleting features/$i";
-       curl --ssl -k ftp://${ftp_ip}:${ftp_port}/$target_dir/features/${i} -u "$user:$password" -O --quote "DELE $target_dir/features/${i}"
+       curl --ssl -k ftp://"$user:$password"@"${ftp_ip}:${ftp_port}/$target_dir/features/${i}" -O --quote "DELE $target_dir/features/${i}"
 };
 done;
 
-for i in `curl --ssl -k -s -l ftp://"$user":"$password"@"$ftp_ip/$target_dir/releases/" | grep .jar`; do
+for i in `curl --ssl -k -s -l ftp://"$user":"$password"@"$ftp_ip/$target_dir/releases/" | grep .zip`; do
 {
        echo "deleting releases/$i";
-       curl --ssl -k ftp://${ftp_ip}:${ftp_port}/$target_dir/features/${i} -u "$user:$password" -O --quote "DELE $target_dir/releases/${i}"
+       curl --ssl -k ftp://"$user:$password"@"${ftp_ip}:${ftp_port}/$target_dir/releases/${i}" -O --quote "DELE $target_dir/releases/${i}"
 };
 done;
